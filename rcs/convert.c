1d9c72b42755a8cf448c968a55bb4899e9354719
Carsten Schlote <schlote@vahanus.net>
1996-02-24 11:19:50 +0000

    Initial revision

---
+++ rcs/convert.c
@@ -0,0 +1,24 @@
+
+/*-------------------------------------------------------------------------------
+**  /\  |\     Silicon Department      Telefax     06404-64760
+**  \_ o| \_ _  Software Entwicklung    Telefon        06404-7996
+**    \|| |_)|)   Carsten Schlote         Egelseeweg 52     35423 Lich
+** \__/||_/\_|     Branko Mikiç            Limmerstrasse 10   30451 Hannover
+**-------------------------------------------------------------------------------
+** ALL RIGHTS ON THIS SOURCES RESERVED TO SILICON DEPARTMENT SOFTWARE
+**
+** $Id$
+**
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define  USE_BUILTIN_MATH
+#include <string.h>
+
+
+void main(void)
+{
+FILE
+}
+
