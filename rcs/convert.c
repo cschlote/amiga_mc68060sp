d4a78f7b940523764cd3231e58a751490abb0ca1
Carsten Schlote <schlote@vahanus.net>
1996-02-24 11:41:56 +0000

    -

--- rcs/convert.c
+++ rcs/convert.c
@@ -7,7 +7,7 @@
 **-------------------------------------------------------------------------------
 ** ALL RIGHTS ON THIS SOURCES RESERVED TO SILICON DEPARTMENT SOFTWARE
 **
-** $Id$
+** $Id: convert.c 1.0 1996/02/24 11:19:50 schlote Exp schlote $
 **
 */
 
@@ -16,9 +16,53 @@
 #define  USE_BUILTIN_MATH
 #include <string.h>
 
+#include <exec/types.h>
 
-void main(void)
+void TranslateBuffer(TEXT *buffer)
 {
-FILE
+int i,x;
+TEXT *p,work[256];
+
+	for ( p=buffer; *p!=0; p++ )
+	{
+      switch( *p )
+      {
+      case '#'	:	*p='*'; break;
+      }
+	}
+
+	if ( sscanf(buffer," short 0x%x %s\n",&x, work) == 2 )
+		sprintf(buffer,"  	dc.w	$%x		%s\n",x,work);
+}
+
+
+
+void main(int argc, char *argv[])
+{
+FILE *in, *out;
+TEXT buffer[256];
+
+	printf("Intel Motorola 2 Standard Assembly Converter.\n\n");
+
+	printf("Open file '%s'\n",argv[1]);
+	if ( in = fopen( argv[1],"r") )
+	{
+		printf("Open file '%s'\n",argv[1]);
+		if ( out = fopen( argv[2],"w") )
+		{
+      	while ( fgets(buffer,256,in) )
+      	{
+            TranslateBuffer(buffer);
+            if ( !fputs( buffer, out) )
+            {
+            	printf("*** error on file write\n");
+            	break;
+            }
+      	}
+
+			fclose(out);
+		}
+		fclose(in);
+	}
 }
 
